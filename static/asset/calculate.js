5e4b43ccc46137a44781ff90fa3c68fae2b346d7
onfranciis <onukwuf@gmail.com>
2024-04-25 03:19:27 +0100

    fix: pre fix ui for new models

--- static/asset/calculate.js
+++ static/asset/calculate.js
@@ -3,10 +3,10 @@ import { getCookie } from "./util.js";
 const form = document.getElementById("form");
 const loading = document.getElementById("loading");
 const result = document.getElementById("result");
-const fvw = parseFloat(document.getElementById("fvw").value || 0);
-const cm = parseFloat(document.getElementById("cm").value || 0);
-const mc = parseFloat(document.getElementById("mc").value || 0);
-const vsc = parseFloat(document.getElementById("vsc").value || 0);
+const fvw = document.getElementById("fvw").value || "0";
+const cm = document.getElementById("cm").value || "0";
+const mc = document.getElementById("mc").value || "0";
+const vsc = document.getElementById("vsc").value || "0";
 const csrfToken = getCookie("csrftoken");
 
 const handleSubmit = async (e) => {
@@ -17,27 +17,29 @@ const handleSubmit = async (e) => {
 
     console.log({ fvw, cm, mc, vsc });
 
-    const response = await fetch("http://127.0.0.1:8000/calculate-biogas/", {
-      method: "POST",
-      headers: {
-        "X-CSRFToken": csrfToken, // Include CSRF token
-        Origin: "http://127.0.0.1:8000",
-      },
-      body: {
-        ratio_fvw: fvw,
-        ratio_cow_manure: cm,
-        moisture_content: mc,
-        vs_content: vsc,
-      },
-    });
+    const response = await fetch(
+      "http://127.0.0.1:8000/calculate_percentage_of_fat/",
+      {
+        method: "POST",
+        headers: {
+          "X-CSRFToken": csrfToken, // Include CSRF token
+          Origin: "http://127.0.0.1:8000",
+        },
+        body: {
+          weight_of_flask: fvw,
+          weight_of_oil: cm,
+          weight_of_empty_flask: mc,
+          weight_of_sample: vsc,
+        },
+      }
+    );
 
     const jsonResponse = await response.json();
 
-    result.innerHTML = jsonResponse.response_message;
+    result.innerHTML = jsonResponse.response_message || jsonResponse.error;
 
     loading.style.display = "none";
     result.style.display = "block";
-
   } catch (err) {
     console.log(err);
   }
